a0193cd4304068d1e5b325030df88f1e142cb909
engstk <eng.stk@sapo.pt>
2014-08-26 19:36:31 +0100

    blu_plug: improvements and fixes * Driver recode and rework * Improve suspend/resume routines * Improve load calculations and plu/unplug routines * Better work and queue management * Fix merge derps and unplug if down_timer_cnt is set lower than up_timer_cnt thanks @kagwind * Use original stratosk load calculation, link to cpu governor * Add debug stuff * Add min_cpu_offscreen tunable * Other  misc. that I forgot ;p

--- drivers/cpufreq/cpufreq_intelliactive.c
+++ drivers/cpufreq/cpufreq_intelliactive.c
@@ -430,6 +430,8 @@ static void cpufreq_interactive_timer(unsigned long data)
 	cpu_load = loadadjfreq / pcpu->target_freq;
 	pcpu->prev_load = cpu_load;
 	boosted = boost_val || now < boostpulse_endtime;
+	
+	cpufreq_notify_utilization(pcpu->policy, cpu_load);
 
 	if (counter < 5) {
 		counter++;
